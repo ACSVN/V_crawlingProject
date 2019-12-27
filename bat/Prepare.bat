6c33be856b085e44bd979585b88a3cc5cc8ceeca
anoseaddix <anoseaddix@users.noreply.github.com>
2019-12-27 17:16:47 +0700

    一旦全消し

--- bat/Prepare.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-pushd %~dp0..
-set _PROJECT_DIR=%CD%
-
-del %_PROJECT_DIR%\results\*.txt
-del %_PROJECT_DIR%\results\*.csv
-rd /S /Q %_PROJECT_DIR%\results\capture
-rd /S /Q %_PROJECT_DIR%\results\HTTrack
